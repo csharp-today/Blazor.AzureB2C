400cb2d0777c377280c5420900eee44cfbe1e811
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2019-10-07 15:33:22 +0200

    Add DI extension method

---
+++ source/CSharpToday.Blazor.AzureB2C/B2CExtensions.cs
@@ -0,0 +1,10 @@
+﻿using CSharpToday.Blazor.AzureB2C.Url;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class B2CExtensions
+    {
+        public static IServiceCollection AddB2CAuthorization(this IServiceCollection services) => services
+            .AddSingleton<IUrlTokenReader, UrlTokenReader>();
+    }
+}
