b691d5bd0ad0971dc7310a53bf8d480ba7934e9b
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2019-10-10 17:13:33 +0200

    Test DI extension

---
+++ source/CSharpToday.Blazor.AzureB2C.Test/B2CExtensionsTest.cs
@@ -0,0 +1,25 @@
+﻿using LucidCode;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace CSharpToday.Blazor.AzureB2C.Test
+{
+    [TestClass]
+    public class B2CExtensionsTest : BaseTest
+    {
+        [TestMethod]
+        public void Build_All_Services() => LucidTest
+            .Arrange(() => new ServiceCollection())
+            .Act(services => services.AddB2CAuthorization(Get<IB2CConfig>()))
+            .Assert(services =>
+            {
+                var provider = services.BuildServiceProvider();
+                foreach (var service in services)
+                {
+                    var obj = provider.GetService(service.ServiceType);
+                    obj.ShouldNotBeNull();
+                }
+            });
+    }
+}
